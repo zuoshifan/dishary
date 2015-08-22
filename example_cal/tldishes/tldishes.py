8cff1b5ecca0699494b0d0417d2c5c5016684b2d
zuoshifan <zuoshifan@163.com>
2015-08-22 12:11:59 +0800

    Index change for 16 dishes

--- example_cal/tldishes/tldishes.py
+++ example_cal/tldishes/tldishes.py
@@ -38,7 +38,7 @@ elev = 1504.3 # m
 
 # (+E, +N) coordinates of each antenna in unit m
 dishes_coord = np.loadtxt('16dishes_coord.txt')
-center_coord = dishes_coord[7]
+center_coord = dishes_coord[15] # central antenna coordinate
 dishes_coord -= center_coord
 # antenna positions
 ant_pos_m = np.zeros((dishes_coord.shape[0], 3), dtype=dishes_coord.dtype)
@@ -72,7 +72,6 @@ def get_aa(freqs):
     # location = prms['loc']
     antennas = []
     pointing = prms['pointing']
-    # nants = len(prms['antpos'])
     assert(len(prms['delays']) == nants and len(prms['offsets']) == nants and len(prms['bp_r']) == nants and len(prms['bp_i']) == nants and len(prms['amps']) == nants)
     for pos, dly, off, bp_r, bp_i, amp in zip(prms['antpos'], prms['delays'], prms['offsets'], prms['bp_r'], prms['bp_i'], prms['amps']):
         # antennas.append(ap.pol.Antenna(pos[0],pos[1],pos[2], beam, phsoff=[dly, off], bp_r=bp_r, bp_i=bp_i, amp=amp, pointing=pointing))
